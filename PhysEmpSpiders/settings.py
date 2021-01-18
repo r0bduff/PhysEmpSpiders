e17f7d85d6be5475e3c2d240c9ee96113bb4044b
r0bduff <rduff154@gmail.com>
2021-01-18 17:45:23 -0600

    Created PMhospitalListings and PMAgencyListings

--- PhysEmpSpiders/settings.py
+++ PhysEmpSpiders/settings.py
@@ -66,9 +66,9 @@ CONCURRENT_REQUESTS = 10
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'PhysEmpSpiders.pipelines.PhysempspidersPipeline': 300,
-}
+#ITEM_PIPELINES = {
+   #'PhysEmpSpiders.pipelines.PhysempspidersPipeline': 300,
+#}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
