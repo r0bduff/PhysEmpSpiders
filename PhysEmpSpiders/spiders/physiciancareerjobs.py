83a646014b2d0cbface5f981638083d29553f6b1
r0bduff <rduff154@gmail.com>
2021-01-22 14:27:18 -0600

    radworking spider finished

---
+++ PhysEmpSpiders/spiders/physiciancareerjobs.py
@@ -0,0 +1,118 @@
+#NOT POSSIBLE TO SPIDER WEBSITE
+
+import scrapy
+from scraper_api import ScraperAPIClient
+from ..items import PhysempspidersItem as Item
+from datetime import datetime
+import re
+
+client = ScraperAPIClient('f2a3c4d1c7d60b6d2eb03c55108e3960')
+
+class PhysiciancareerjobsSpider(scrapy.Spider):
+    name = 'physiciancareerjobs'
+
+    #start_urls = ['http://website.com/']
+    #custom_settings={ 'FEED_URI': "jamaNetwork_%(time)s.csv", 'FEED_FORMAT': 'csv'}
+    def start_requests(self):
+        lastpagenum = 799
+        for i in range(lastpagenum):
+            next_page = 'https://www.physiciancareerjobs.com/candidate/job_search/quick/results/' + str(i)
+            yield scrapy.Request(client.scrapyGet(url= next_page), callback=self.parse)
+        
+    def parse(self, response):
+        for post in response.css(''):
+            try:
+                url = 'POST' 
+                title = 'POST'
+                business_name = 'POST'
+                if(url is not None):
+                    yield scrapy.Request(client.scrapyGet(url= url), callback=self.parse_listing, meta={'url': url, 'title': title, 'business_name': business_name})
+            except Exception as e:
+                print(e)
+
+#Parse listing page
+    def parse_listing(self, response):
+        print('---------------------CHECKING INTERIOR PAGE--------------------------')
+
+        #find emails
+        email = ''
+        findemail = re.search(r'[\w\.-]+@[\w\.-]+', response.css('.job-description').get())
+        if(findemail is not None):
+            email = findemail.group(0)
+
+        #find phone numbers
+        phone = ''
+        findphone = re.search(r'(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?', response.css('.job-description').get())
+        if(findphone is not None):
+            phone = findphone.group(0)
+
+        location = response.css('').get().split(',')
+        if(len(location) == 2):
+            city = location[0]
+            state = location[1].strip()
+        else:
+            state = location[0].strip()
+            city = ''
+       
+        try:
+            job = Item({
+                'title': response.meta['title'],
+                'specialty': '',
+                'hospital_name': '',
+                'job_salary': '',
+                'job_type': '',
+                'job_state': state,
+                'job_city': city,
+                'job_address': '',
+                'date_posted': '',
+                'date_scraped': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                'source_site': 'website',
+                'url': response.meta['url'],
+                'description': '',
+                'business_type': '',
+                'business_name': response.meta['business_name'],
+                'contact_name': '',
+                'contact_number': phone,
+                'contact_email': email,
+                'business_state': state,
+                'business_city': city,
+                'business_address': '',
+                'business_zip': '',
+                'hospital_type': '',
+                'business_website': '',
+                'hospital_id': '',
+                'Ref_num': '',
+            })
+            yield job
+
+        except Exception as e:
+            job = Item({
+                'title': response.meta['title'],
+                'specialty': '',
+                'hospital_name': '',
+                'job_salary': '',
+                'job_type': '',
+                'job_state': '',
+                'job_city': '',
+                'job_address': '',
+                'date_posted': '',
+                'date_scraped': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                'source_site': 'website',
+                'url': response.meta['url'],
+                'description': '',
+                'business_type': '',
+                'business_name': response.meta['business_name'],
+                'contact_name': '',
+                'contact_number': '',
+                'contact_email': '',
+                'business_state': '',
+                'business_city': '',
+                'business_address': '',
+                'business_zip': '',
+                'hospital_type': '',
+                'business_website': '',
+                'hospital_id': '',
+                'Ref_num': '',
+            })
+            print(e)
+            yield job
